781429ba8771a2a35cf314fe05f42519bfe25abf
LaertNeto <32277202+LaertNeto@users.noreply.github.com>
2024-01-08 21:02:25 -0300

    Adiciona as classes de cartas e deck

--- TrucoGame/include/models/Card.h
+++ TrucoGame/include/models/Card.h
@@ -1,19 +1,44 @@
 #pragma once 
+#include <SFML/Graphics.hpp>
+#include <string>
 
 namespace TrucoGame {
 
     namespace Models {
+        enum Suit { Clubs, Spades, Diamonds, Hearts };
 
         class Card {
         private:
             int value;
-            string suit;
-            //image image;
+            Suit suit;
+            sf::Sprite sprite;
+            sf::Texture texture;
         public:
-            Card(int value, string suit);
+            Card(int value, Suit suit);
             ~Card();
-            string getSuit();
-            int getValor();
+            Suit getSuit();
+            int getValue();
+            sf::Sprite* getSprite();
+            std::string getSpritePath();
+        };
+
+        class ClubsCard : public Card {
+        public:
+            ClubsCard(int value) : Card(value, Suit::Clubs) {}
+        };
+
+        class SpadesCard : public Card {
+        public:
+            SpadesCard(int value) : Card(value, Suit::Spades) {}
+        };
+
+        class DiamondsCard : public Card {
+        public:
+            DiamondsCard(int value) : Card(value, Suit::Diamonds) {}
+        };
+        class HeartsCard : public Card {
+        public:
+            HeartsCard(int value) : Card(value, Suit::Hearts) {}
         };
     }
 }
